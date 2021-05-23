d941990a7a26c7bff9ec2832fb303e9f267a3d2a
tuantuanPanda <67112851+tuantuanPanda@users.noreply.github.com>
2021-05-23 11:14:38 +0800

    idk

---
+++ .vscode/bot.py
@@ -0,0 +1,10 @@
+import discord
+From discord.ext import commands
+
+bot=commands.Bot[command_prefix='/']
+
+@bot.event
+async deP on_ready[]:
+    print[">>Bot is online<<"]
+
+bot.run['ODQ1NDU3NzAyODg0NjA1OTUz.YKhP2A.3d6Z_GSwgCQEAcxmuIc3OJvh44g']
